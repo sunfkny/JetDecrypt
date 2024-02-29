2d1efd16f073c23c62c0ceae043af462f2e67d92
sunfkny <30853461+sunfkny@users.noreply.github.com>
2024-02-29 17:25:25 +0800

    refactor

--- README.md
+++ README.md
@@ -3,18 +3,18 @@ You can use this tool on Windows to recover the master password used to encrypt 
 
 # Usage
 ```
-pipenv install
-pipenv run getpw.py path_to_pdb.pwd
+pip install pywin32 pycryptodome
+python getpw.py path_to_c.pwd
 ```
 
 # Background
 The KeePass database used on Windows by tools like DataGrip to store passwords is encrypted with a password that itself is encrypted twice. It is first encrypted with a static password within the IntelliJ source code, and then again with the Windows Credential Store (i.e. the current user's Windows credentials).
 
-This program reverses that process for a given `pdb.pwd` file.
+This program reverses that process for a given `c.pwd` file.
 
 # Tips
-- This has been tested with Jetbrains DataGrip 2019.1. Other JetBrains products and/or versions of DataGrip may not be compatible with this tool.
-- The `c.kdbx` and `pdb.pwd` files for me were saved in `%USERPROFILE%\.DataGrip2019.1\config`, but you should find the path that is correct for your version of DataGrip.
+- This has been tested with Jetbrains DataGrip 2023.3. Other JetBrains products and/or versions of DataGrip may not be compatible with this tool.
+- The `c.kdbx` and `c.pwd` files for me were saved in `%APPDATA%\JetBrains\DataGrip2023.3`, but you should find the path that is correct for your version of DataGrip.
 - Once the password is recovered, you can simply double click the `c.kdbx` file to open it in KeePass, and then paste the password provided in. This of course requires KeePass to be installed.
 - You should not tamper with the KeyPass DB! Use this only to recover passwords you have forgotten. The password IDs correspond to IDs elsewhere in the XML configuration files.
 - If you don't know what this tool is or does then I recommend you don't use it. It is for advanced users only.
--- getpw.py
+++ getpw.py
@@ -1,59 +1,67 @@
+import base64
 import binascii
+import pathlib
 import struct
 import sys
 
 import win32crypt
-
 from Crypto.Cipher import AES
 from Crypto.Util.Padding import unpad
 
 # The Master Password is encrypted with this static key
-# https://github.com/JetBrains/intellij-community/blob/93dde45a41a9d152e01a151d7ae411faa3cf7f61/platform/credential-store/src/EncryptionSupport.kt#L16
+# https://github.com/JetBrains/intellij-community/blob/e84b32f0620126b0e2b3a3f477cda8c1c9b5b4d2/platform/credential-store/src/EncryptionSupport.kt#L16-L19
 CONTAINER_KEY = "Proxy Config Sec"
+# https://github.com/cdeptula/intellij-community/blob/41de88111b8d8b65a69e678f0cc1e9b03753a92b/platform/credential-store/src/keePass/masterKey.kt#L162
+ENCRYPT_VALUE_START = "value: !!binary ".encode()
 
 
 def decrypt(path):
     print("[+] Reading encrypted container as bytes")
-    enc_bytes = []
-    with open(path, "rb") as f:
-        byte = f.read(1)
-        while byte:
-            enc_bytes.append(byte)
-            byte = f.read(1)
+    master_key_file_content = pathlib.Path(path).read_bytes()
 
-    enc_bytes_array = b''.join(enc_bytes)
+    master_key_encoded = None
+    for line in master_key_file_content.splitlines():
+        if line.startswith(ENCRYPT_VALUE_START):
+            master_key_encoded = line.replace(ENCRYPT_VALUE_START, b"")
+            break
+    if not master_key_encoded:
+        print("Master Key encoded value not found")
+        return
+
+    enc_bytes_array = base64.b64decode(master_key_encoded)
 
     print("[+] Encrypted bytes:")
     print(binascii.hexlify(enc_bytes_array))
 
     print("[+] Decrypted bytes from Credential Manager")
-    decrypted_container = win32crypt.CryptUnprotectData(
-        enc_bytes_array, None, None, None, 0
-    )[1]
+    decrypted_container = win32crypt.CryptUnprotectData(enc_bytes_array, Flags=0)[1]
     print(binascii.hexlify(decrypted_container))
 
-    iv_length = struct.unpack('>i', decrypted_container[:4])[0]
+    iv_length = struct.unpack(">i", decrypted_container[:4])[0]
     print("IV Length: " + str(iv_length))
 
-    iv = decrypted_container[4:4 + iv_length]
+    iv = decrypted_container[4 : 4 + iv_length]
     print("[+] IV: " + str(binascii.hexlify(iv)))
 
-    data = decrypted_container[4 + iv_length:]
+    data = decrypted_container[4 + iv_length :]
     print("[+] Payload length: " + str(len(data)))
 
-    key = CONTAINER_KEY.encode('ascii')
+    key = CONTAINER_KEY.encode("ascii")
     cipher = AES.new(key, AES.MODE_CBC, iv=iv)
-    result = unpad(cipher.decrypt(data), AES.block_size).decode('ascii')
+    result = unpad(cipher.decrypt(data), AES.block_size).decode("ascii")
 
     print("-------------------------------------")
-    print("[+] DECODED PASSWORD")
+    print("[+] Master password")
     print(result)
+    print("-------------------------------------")
+    print("You can open c.kdbx with KeePass and this password")
+    print("KeePass Downloads https://keepass.info/download.html")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # Program Entry Point
     if len(sys.argv) != 2:
-        print("This program takes one parameter: the path to pdb.pwd.")
+        print("This program takes one parameter: the path to c.pwd")
         exit(1)
 
     decrypt(sys.argv[1])
